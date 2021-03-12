41ea95e73cf49b3c770f26d595555f0d3935493f
Mads Klejs Vilhelmsen <mads019f@edu.ucl.dk>
2021-03-12 15:25:25 +0100

    signOut knap
    fungere muligvis

--- TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/MainActivity.kt
+++ TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.tv2app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.tv2app.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Set up navigation controller
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        // Get an instance of NavController from the NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Show a title in the app bar based off of the destination's label, and
+        // display the Up(←) button whenever you're not on a top-level destination.
+        setupActionBarWithNavController(navController)
     }
 }
--- TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/StartFragment.kt
+++ TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/StartFragment.kt
@@ -1,29 +1,67 @@
 package com.example.tv2app.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.tv2app.R
+import com.example.tv2app.databinding.ActivityMenuBinding
+import com.example.tv2app.databinding.FragmentStartBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 /**
  * This is the first screen of the TV2 app. The user can choose between 3 buttons in a menu.
  */
 class StartFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    // Binding object instance with access to the views in the start_fragment.xml layout
+    private lateinit var binding: FragmentStartBinding
 
-    }
+    lateinit var auth : FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        // Inflate the layout XML file and return a binding object instance
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_start, container, false)
+        return binding.root
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Binding Elements Here
+        val signOut = binding.signOut
+        
+        //OnClickListeners
+        signOut.setOnClickListener{ signOut() }
+
+
+    }
+
+
+    //TODO signOut() virker ikke i fragment. Move this to UserRepository
+    private fun signOut(){
+        auth.signOut()
+        Toast.makeText(context, "Signed Out", Toast.LENGTH_SHORT).show()
+        val intent = Intent(activity, LoginActivity::class.java) // Fragments is not of context type = need the parent activity
+        activity?.startActivity(intent)
+    }
+
+    private fun goToLeaderboard(){
+        findNavController().navigate(R.id.action_startFragment_to_leaderboardFragment)
+    }
+
 
 
 
