1cad3767da0f928903ef6070d886064bf7257131
Mads Klejs Vilhelmsen <mads019f@edu.ucl.dk>
2021-03-17 13:05:22 +0100

    Opdateret tilbage-knap i Action bar
    overskrevet onSupportNavigateUp() metoden i MainActivity

--- TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/MainActivity.kt
+++ TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.tv2app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.tv2app.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -15,10 +19,14 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         // Get an instance of NavController from the NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         // Show a title in the app bar based off of the destination's label, and
         // display the Up(←) button whenever you're not on a top-level destination.
         setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
