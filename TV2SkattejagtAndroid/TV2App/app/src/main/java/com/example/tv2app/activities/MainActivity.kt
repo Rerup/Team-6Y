2765ea6d483147854a58f4d438e62fd74e5bd95c
Mads Klejs Vilhelmsen <mads019f@edu.ucl.dk>
2021-03-17 17:26:11 +0100

    Tilføjet overflow menu til app bar

--- TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/MainActivity.kt
+++ TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.tv2app.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.tv2app.R
+import com.example.tv2app.viewmodels.UserViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var navController: NavController
+
+    private val userViewModel: UserViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +34,35 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         setupActionBarWithNavController(navController)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.indstillinger -> {
+                Toast.makeText(this,"Indstillinger", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.logud -> {
+                signOut()
+                return true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun signOut(){
+        userViewModel.signOutUser()
+        Toast.makeText(this, "Signed Out", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
