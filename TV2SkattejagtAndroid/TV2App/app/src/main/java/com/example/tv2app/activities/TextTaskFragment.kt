325fd2ed1c4d18bd9106087544115269df46c79b
Mads Klejs Vilhelmsen <mads019f@edu.ucl.dk>
2021-04-06 19:21:46 +0200

    UI til TextTask + lidt Databinding

--- TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/TextTaskFragment.kt
+++ TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/TextTaskFragment.kt
@@ -1,30 +1,35 @@
 package com.example.tv2app.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import com.example.tv2app.R
 import com.example.tv2app.databinding.FragmentProfileBinding
 import com.example.tv2app.databinding.FragmentTextTaskBinding
+import com.example.tv2app.models.TextTask
+import com.example.tv2app.models.User
 import com.example.tv2app.viewmodels.TaskViewModel
 import com.example.tv2app.viewmodels.UserViewModel
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class TextTaskFragment : Fragment() {
 
-    lateinit var auth : FirebaseAuth
+    lateinit var auth: FirebaseAuth
 
-    private val taskViewModel : TaskViewModel by activityViewModels()
+    private val taskViewModel: TaskViewModel by activityViewModels()
+
     private lateinit var binding: FragmentTextTaskBinding
-    private lateinit var db : FirebaseDatabase
-    private lateinit var ref : DatabaseReference
+    private lateinit var db: FirebaseDatabase
+    private lateinit var ref: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,14 +37,49 @@ class TextTaskFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
 
+        //TODO Jacob what is this??
+        //auth = FirebaseAuth.getInstance()
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_task, container, false)
+        // Inflate the layout XML file and return a binding object instance
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+
+
+        //TODO Jacobo er der bruge for dette?
+        //Track current User
+        //val currentUser = auth.currentUser?.uid ?:""
+
+
+        fun getCurrentTask(id: String) {
+            db = FirebaseDatabase.getInstance()
+            ref = db.reference.child("Tasks")
+            ref.child(id).addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    val task = snapshot
+                    val currentTaskObject = task.getValue(TextTask::class.java)
+                    binding.points = getString(R.string.point_amount, currentTaskObject?.point)
+                            ?: ""
+                    binding.question = currentTaskObject?.description ?: ""
+
+                    Log.i("DB READ", "point: ${binding.points}, question: ${binding.question}")
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    //Handle the error
+                    Log.i("DB READ", error.message)
+                }
+            })
+        }
+
+        //TODO need fix
+        //getCurrentTask(currentTask)
+
+        return binding.root
     }
-
 
 }
