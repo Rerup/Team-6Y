526367b96a3a8d51ee5f446f5912885b7dbdc673
Mads Klejs Vilhelmsen <mads019f@edu.ucl.dk>
2021-05-04 14:00:21 +0200

    Tilføjelser til leaderboard + tilbage knap

--- TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/LeaderboardActivity.kt
+++ TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/LeaderboardActivity.kt
@@ -4,9 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil.setContentView
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tv2app.R
@@ -14,8 +13,6 @@ import com.example.tv2app.adapter.LeaderboardAdapter
 import com.example.tv2app.models.User
 import com.example.tv2app.viewmodels.UserViewModel
 import com.google.firebase.auth.FirebaseAuth
-import org.koin.androidx.viewmodel.compat.SharedViewModelCompat.sharedViewModel
-import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LeaderboardActivity : AppCompatActivity() {
@@ -31,6 +28,7 @@ class LeaderboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leaderboard)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         auth = FirebaseAuth.getInstance()
 
@@ -42,7 +40,7 @@ class LeaderboardActivity : AppCompatActivity() {
         userList = getUserList()
         leaderboardRecyclerView.adapter = LeaderboardAdapter(userList)
 
-
+        showCurrentPlayerData(userList)
     }
 
     override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
@@ -69,6 +67,35 @@ class LeaderboardActivity : AppCompatActivity() {
 
     }
 
+    fun showCurrentPlayerData(userList : ArrayList<User?>){
+        //Track current User
+        val currentUser = auth.currentUser?.uid ?:""
+
+        //Get current user values with currently logged in userID.
+        userViewModel.getCurrentUser(currentUser)
+
+        //Fetches the user object so that we can populate the view.
+        val userObject = userViewModel.fetchUserToView()
+
+        //Binding view elements with corresponding values.
+        val fullName : TextView = findViewById(R.id.cp_fullName)
+        fullName.text = userObject?.fullName?:""
+
+        val points : TextView = findViewById(R.id.cp_score)
+        points.text = userObject?.totalPoints?.toString()?:"0"
+
+        val department : TextView = findViewById(R.id.cp_department)
+        department.text = userObject?.departmentId?:""
+
+        val ranking : TextView = findViewById(R.id.cp_rank)
+        ranking.text = findIndex(userList,userObject).toString()
+
+    }
+
+    //TODO Eventuelt flytte til UserViewModel, da metode defineres to steder (LeaderBoardAdapter)
+    private fun findIndex(array : ArrayList<User?>, item : User?) : Int {
+        return array.indexOf(item) + 1
+    }
 
 
 }
