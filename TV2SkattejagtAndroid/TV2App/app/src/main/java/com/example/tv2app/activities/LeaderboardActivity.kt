e334c1efe2d884e80bbca306444812645728c5ef
Rerup <Jaco222r@edu.ucl.dk>
2021-05-25 11:59:55 +0200

    Revert "RecyclerView"
    This reverts commit 34e25de34a9d4e5505a65bd252f485c9f2fed16f.

--- TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/LeaderboardActivity.kt
+++ TV2SkattejagtAndroid/TV2App/app/src/main/java/com/example/tv2app/activities/LeaderboardActivity.kt
@@ -5,11 +5,9 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
-import android.widget.Adapter
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tv2app.R
@@ -17,15 +15,14 @@ import com.example.tv2app.adapter.LeaderboardAdapter
 import com.example.tv2app.models.User
 import com.example.tv2app.viewmodels.UserViewModel
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_leaderboard.*
 import kotlinx.coroutines.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LeaderboardActivity : AppCompatActivity() {
 
-
+    private lateinit var leaderboardRecyclerView : RecyclerView
+    private lateinit var userList : ArrayList<User?>
     lateinit var auth : FirebaseAuth
-    private val adapter = LeaderboardAdapter()
 
     private val userViewModel : UserViewModel by viewModel()
 
@@ -38,50 +35,33 @@ class LeaderboardActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         //Retrieve List of Users
-        getUserList()
+        userList = getUserList()
 
         //Shows the current rank of the logged in User.
-        showCurrentPlayerData()
+        showCurrentPlayerData(userList)
 
         //Initialize RecyclerView
         initRecyclerView()
 
         //Notify RecyclerView that data has changed since being initialized.
-        recyclerLeaderboard.adapter?.notifyDataSetChanged()
+        leaderboardRecyclerView.adapter?.notifyDataSetChanged()
 
 
     }
 
     override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        recyclerLeaderboard.adapter = adapter
-
-
-        //Load data to view here
-        userViewModel.userLiveData.observe(this) { userObject ->
-
-            //Binding view elements with corresponding values.
-            val fullName: TextView = findViewById(R.id.cp_fullName)
-            fullName.text = userObject?.fullName ?: ""
-
-            val points: TextView = findViewById(R.id.cp_score)
-            points.text = userObject?.totalPoints?.toString() ?: "0"
-
-            val department: TextView = findViewById(R.id.cp_department)
-            department.text = userObject?.departmentId ?: ""
-
-            //val ranking: TextView = findViewById(R.id.cp_rank)
-            //ranking.text = userViewModel.findUserIndexInLeaderboard(userList, userObject).toString()
-        }
-
-        userViewModel.leaderboard.observe(this) {users ->
-            adapter.updateUI(users)
-        }
 
         return super.onCreateView(name, context, attrs)
     }
 
     private fun initRecyclerView(){
-        //leaderboardRecyclerView = findViewById(R.id.recyclerLeaderboard)
+        leaderboardRecyclerView = findViewById(R.id.recyclerLeaderboard)
+        leaderboardRecyclerView.layoutManager = LinearLayoutManager(this)
+        leaderboardRecyclerView.setHasFixedSize(false)
+
+        //Set adapter
+        leaderboardRecyclerView.adapter = LeaderboardAdapter(userList)
+
 
     }
 
@@ -90,11 +70,14 @@ class LeaderboardActivity : AppCompatActivity() {
         userViewModel.userList.clear()
     }
 
-    private fun getUserList()  {
-        userViewModel.inflateLeaderboard()
+    private fun getUserList() : ArrayList<User?> {
+        val list = userViewModel.inflateLeaderboard()
+        list.sortByDescending { it?.totalPoints ?:0 }
+        return list
+
     }
 
-    private fun showCurrentPlayerData(){
+    private fun showCurrentPlayerData(userList : ArrayList<User?>){
         //Track current User
         val currentUser = auth.currentUser?.uid ?:""
 
@@ -103,6 +86,19 @@ class LeaderboardActivity : AppCompatActivity() {
 
         //Fetches the user object so that we can populate the view.
         val userObject = userViewModel.fetchUserToView()
+
+        //Binding view elements with corresponding values.
+        val fullName : TextView = findViewById(R.id.cp_fullName)
+        fullName.text = userObject?.fullName?:""
+
+        val points : TextView = findViewById(R.id.cp_score)
+        points.text = userObject?.totalPoints?.toString()?:"0"
+
+        val department : TextView = findViewById(R.id.cp_department)
+        department.text = userObject?.departmentId?:""
+
+        val ranking : TextView = findViewById(R.id.cp_rank)
+        ranking.text = userViewModel.findUserIndexInLeaderboard(userList,userObject).toString()
 
     }
 
